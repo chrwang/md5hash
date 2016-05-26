4a37631c2d2171da6953429f319ca4f8fd64d5ac
Eric Shen <eric.shen2000@gmail.com>
2016-05-26 15:04:25 -0400

    Make f,g,h,i methods

--- hashbrown5/src/hashbrown5/MD5.java
+++ hashbrown5/src/hashbrown5/MD5.java
@@ -19,4 +19,59 @@ public class MD5 {
 		
 		// TODO: Handle padding
 	}
+	
+	private byte[] f(byte[] b, byte[] c, byte[] d){
+		if(b.length !=4 || c.length != 4 || d.length != 4){
+			throw new IllegalArgumentException(":'(  -- f");
+		}
+		
+		
+		byte[] output = new byte[4];
+		for(int i = 0; i < 4; i++){
+			output[i] = (byte) ((b[i]&c[i]) | ((~b[i])&d[i]));
+		}
+		
+		return output;
+	}
+	
+	private byte[] g(byte[] b, byte[] c, byte[] d){
+		if(b.length !=4 || c.length != 4 || d.length != 4){
+			throw new IllegalArgumentException(":'(  -- h");
+		}
+		
+		byte[] output = new byte[4];
+		for(int i = 0; i < 4; i++){
+			output[i] = (byte) ((b[i]&d[i]) | (c[i]&(~d[i])));
+		}
+		
+		return output;
+		
+	}
+	
+	private byte[] h(byte[] b, byte[] c, byte[] d){
+		if(b.length !=4 || c.length != 4 || d.length != 4){
+			throw new IllegalArgumentException(":'(  -- g");
+		}
+		
+		byte[] output = new byte[4];
+		for(int i = 0; i < 4; i++){
+			output[i] = (byte) (b[i]^c[i]^d[i]);
+		}
+		
+		return output;
+			
+	}
+	
+	private byte[] i(byte[] b, byte[] c, byte[] d){
+		if(b.length !=4 || c.length != 4 || d.length != 4){
+			throw new IllegalArgumentException(":'(  -- i");
+		}
+		
+		byte[] output = new byte[4];
+		for(int i = 0; i < 4; i++){
+			output[i] = (byte) (c[i] ^ (b[i] | (~d[i])));
+		}
+		
+		return output;
+	}
 }
